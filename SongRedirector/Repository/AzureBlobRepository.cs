28a7339cfe1563dcefdb1d9eb71715446201b0c9
Martin Thaller <thaller.martin@gmail.com>
2019-03-24 00:11:08 +0100

    azure blob storage

---
+++ SongRedirector/Repository/AzureBlobRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+
+namespace SongRedirector.Repository
+{
+    public class AzureBlobRepository : FileRepositoryBase
+    {
+        private BlobStorageSettings settings;
+        private CloudBlobClient client;
+        private CloudBlobContainer container;
+
+        public AzureBlobRepository(IOptions<BlobStorageSettings> options)
+        {
+            settings = options.Value;
+
+            if (!CloudStorageAccount.TryParse(settings.ConnectionString, out var account))
+            {
+                throw new Exception("No valid connection string");
+            }
+            client = account.CreateCloudBlobClient();
+            container = client.GetContainerReference(settings.ContainerName);
+        }
+
+
+
+        protected override Stream GetFileStream(string configName)
+        {
+            var blob = container.GetBlockBlobReference(configName + ".songs");
+            var memStream = new MemoryStream();
+            blob.DownloadToStreamAsync(memStream).Wait();
+            memStream.Position = 0;
+            return memStream;
+
+        }
+    }
+}
--- SongRedirector/Startup.cs
+++ SongRedirector/Startup.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,7 +21,8 @@ namespace SongRedirector
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSingleton<ILinkRepository, EmbeddedFileRepository>();
+            services.Configure<BlobStorageSettings>(options => Configuration.GetSection("BlobStorageSettings").Bind(options));
+            services.AddSingleton<ILinkRepository, AzureBlobRepository>();
             services.AddSingleton<ILinkProvider, RandomLinkProvider>();
         }
 
