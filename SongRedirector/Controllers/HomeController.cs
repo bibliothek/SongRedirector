4b7609f2cc9fed6c403e264d41295684a7bf0e5b
Martin Thaller <thaller.martin@gmail.com>
2019-03-24 00:52:51 +0100

    list stuff

--- SongRedirector/Controllers/HomeController.cs
+++ SongRedirector/Controllers/HomeController.cs
@@ -1,20 +1,37 @@
 ﻿using SongRedirector.Services;
 using Microsoft.AspNetCore.Mvc;
+using SongRedirector.Repository;
+using System.Text;
+using System.Linq;
+using System;
 
 namespace SongRedirector.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILinkProvider linkProvider;
+        private readonly ILinkRepository linkRepository;
 
-        public HomeController(ILinkProvider linkProvider)
+        public HomeController(ILinkProvider linkProvider, ILinkRepository linkRepository)
         {
             this.linkProvider = linkProvider;
+            this.linkRepository = linkRepository;
         }
         public IActionResult Index([FromQuery] string tenant = "")
         {
             var uri = linkProvider.GetLink(tenant);
             return Redirect(uri);
+        }
+
+        public IActionResult List([FromQuery] string tenant = "")
+        {
+            var config = linkRepository.GetConfig(tenant);
+            var sb = new StringBuilder();
+            foreach(var link in config.Links)
+            {
+                sb.Append($"{link.DisplayName}: {link.Uri} (Probability: {link.Probability}){Environment.NewLine}");
+            }
+            return Content(sb.ToString());
         }
 
     }
