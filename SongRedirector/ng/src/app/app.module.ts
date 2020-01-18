65a086f6baf961c56927fcc536c28dd1681b3b5f
Martin Thaller <thaller.martin@gmail.com>
2020-01-18 10:37:59 +0100

    nav

--- SongRedirector/ng/src/app/app.module.ts
+++ SongRedirector/ng/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatSelectModule, MatBadgeModule} from '@angular/material';
+import { MatButtonModule, MatCardModule, MatSelectModule, MatBadgeModule, MatMenuModule} from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -22,7 +22,7 @@ import { LinkEffects } from './link.effects';
   declarations: [
     AppComponent,
     NavComponent,
-    EmbeddedVideoComponent
+    EmbeddedVideoComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,6 +32,8 @@ import { LinkEffects } from './link.effects';
     MatCardModule,
     MatIconModule,
     MatSelectModule,
+    MatListModule,
+    MatMenuModule,
     MatBadgeModule,
     LayoutModule,
     HttpClientModule,
--- SongRedirector/ng/src/app/nav/nav.component.ts
+++ SongRedirector/ng/src/app/nav/nav.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { State } from '../reducers';
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { fetchConfigNames, selectConfig } from '../link.actions';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from "@angular/core";
+import { State } from "../reducers";
+import { Store, select } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { fetchConfigNames, selectConfig } from "../link.actions";
+import {
+  Breakpoints,
+  BreakpointObserver,
+  MediaMatcher
+} from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.scss"]
 })
-export class NavComponent implements OnInit {
-  
+export class NavComponent implements OnInit, OnDestroy {
   configNames$: Observable<string[]>;
-  
+
   private _currentConfig = "";
 
   ngOnInit(): void {
-    this.configNames$ = this.store.pipe(select('link'), map(link => link.configs.slice().sort()));
-    this.store.pipe(select('link'), map(link => link.selectedConfig)).subscribe((config) => this._currentConfig = config );
+    this.configNames$ = this.store.pipe(
+      select("link"),
+      map(link => link.configs.slice().sort())
+    );
+    this.store
+      .pipe(
+        select("link"),
+        map(link => link.selectedConfig)
+      )
+      .subscribe(config => (this._currentConfig = config));
     this.store.dispatch(fetchConfigNames());
-  }
-
-  onSelect(event) {
-    console.log(event);
   }
 
   get currentConfig(): string {
@@ -31,12 +39,27 @@ export class NavComponent implements OnInit {
   }
 
   set currentConfig(value: string) {
-    if(value === this._currentConfig){
+    if (value === this._currentConfig) {
       return;
     }
-    this.store.dispatch(selectConfig({name: value}));
+    this.store.dispatch(selectConfig({ name: value }));
   }
 
-  constructor(private store: Store<State>) {}
+  mobileQuery: MediaQueryList;
 
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private store: Store<State>
+  ) {
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
